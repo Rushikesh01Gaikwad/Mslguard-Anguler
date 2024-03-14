817ea8062de8ecc109c53aa6f07ab4141f5b183d
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2024-03-14 10:45:20 +0530

    msl guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { MsalBroadcastService, MsalGuardConfiguration, MsalService, MSAL_GUARD_CONFIG } from '@azure/msal-angular';
+import { filter, Subject, takeUntil } from 'rxjs';
+import { InteractionStatus, RedirectRequest } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'mslguard';
+export class AppComponent implements OnInit, OnDestroy {
+
+  loginDisplay:boolean=false
+  isFrame=false
+  private readonly _destroying$ = new Subject<void>();
+
+  constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
+  private msalBroadCastService : MsalBroadcastService,
+  private authService: MsalService)
+  {
+
+  }
+  
+  ngOnInit()
+  {
+    this.isFrame = window !== window.parent && !window.opener;
+    this.msalBroadCastService.inProgress$.pipe
+    (
+      filter((status: InteractionStatus)=>
+        status === InteractionStatus.None
+      ),takeUntil(this._destroying$)
+    ).subscribe(()=>
+    {
+      this.setloginDisplay();
+    })
+  }
+
+  login()
+  {
+    if (this.msalGuardConfig.authRequest)
+    {
+      this.authService.loginRedirect({...this.msalGuardConfig.authRequest} as RedirectRequest);
+    }
+    else
+    {
+      this.authService.loginRedirect();
+    }
+  }
+
+  logout()
+  {
+    this.authService.logoutRedirect(
+      {
+        postLogoutRedirectUri: 'http://localhost:4200/'
+      }
+    );
+  }
+
+  setloginDisplay()
+  {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+  }
+
+  ngOnDestroy():void
+  {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,72 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent} from '@azure/msal-angular'
+import {PublicClientApplication, InteractionType} from '@azure/msal-browser';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+const isIE= window.navigator.userAgent.indexOf('MSIE')>-1||
+window.navigator.userAgent.indexOf('Trident/')>-1
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatDividerModule,
+    MsalModule.forRoot(new PublicClientApplication
+      (
+        {
+          auth:{
+            clientId:'d7b8956e-f217-4fe7-9278-5c2c2a39d55b',
+            redirectUri:'http://localhost:4200/',
+            authority:'https://login.microsoftonline.com/556e6b1f-b49d-4278-8baf-db06eeefc8e9'
+          },
+          cache:
+          {
+            cacheLocation:'localStorage',
+            storeAuthStateInCookie:isIE
+          }
+        }
+      ),
+      {
+        interactionType: InteractionType.Redirect,
+        authRequest:
+        {
+          scopes: ['user.read']
+        }
+      },
+      {
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map(
+          [
+            ['https://graph.microsoft.com/v1.0/me',['user.read']]
+          ]
+        )
+      })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{
+    provide:HTTP_INTERCEPTORS,
+    useClass:MsalInterceptor,
+    multi:true
+  },MsalGuard],
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
